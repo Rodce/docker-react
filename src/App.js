f168d1ddf687b6d948c1fe3d2b64dbb0739c9923
rodce <rodce.adm@gmail.com>
2019-04-13 19:56:03 +0300

    change apt text

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
             HELLO RODCE
+            feature branch
           </p>
           <a
             className="App-link"
